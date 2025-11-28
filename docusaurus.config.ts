05ac3195172195d7d5c9fa8ad03f03f173e2e799
Andris Reinman <andris@reinman.eu>
2025-11-28 20:38:11 +0200

    Configure GitHub Pages deployment for learn.emailengine.app

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,14 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://emailengine.app',
+  url: 'https://learn.emailengine.app',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'postalsys',
-  projectName: 'emailengine',
+  projectName: 'learn.emailengine.app',
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'warn',
 
